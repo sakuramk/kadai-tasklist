254f3de14b6d84b598a380aafa67b30f30d48103
makos <orin1007@gmail.com>
2017-04-25 02:03:29 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,59 +1,36 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  def index
-  @tasks = Task.all.page(params[:page]).per(10)
-  end
-
-  def show
-     
-  end
-
-  def new
-     @task = Task.new
-  end
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'task が正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'task が投稿されませんでした'
-      render :new
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'toppages/index'
     end
   end
 
-  def edit
-  
-  end
-
-  def update
-    
-
-    if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'Task は更新されませんでした'
-      render :edit
-
-    end
-  end
   def destroy
-      
-    @task.destroy
-
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+     @task.destroy
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
 
- private
-  def set_task
-    @task = Task.find(params[:id])
-  end
-  # Strong Parameter
+  private
+
   def task_params
-    params.require(:task).permit(:content, :status)
+    params.require(:task).permit(:content)
   end
+  
+   def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_path
+    end
+    
+   end
 end
